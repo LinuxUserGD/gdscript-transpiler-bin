64bfbdb6c191e8669aea7569f12db1a41f7ced73
LinuxUserGD <hugegameartgd@gmail.com>
2022-07-28 14:56:28 +0200

    remove main-node.gd

--- RichTextLabel.gd
+++ RichTextLabel.gd
@@ -3,6 +3,7 @@ extends HBoxContainer
 const Main := preload("res://main.gd")
 var main = Main.new()
 var _main = main.Main.new()
+var _self : String = ""
 
 func _on_text_edit_text_changed():
-	$TextEdit2.set_text(_main.transpile($TextEdit.get_text()))
+	$TextEdit2.set_text(_main.transpile(_self, $TextEdit.get_text()))
--- main-node.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-#const Main := preload("res://main.gd")
-#var main = Main.new()
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-	#main._init()
