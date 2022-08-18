e134b7f30277700b562c71d2aad514d40d0537c1
Linux User <linuxusergd@noreply.codeberg.org>
2022-08-19 00:52:10 +0200

    „.github/workflows/api.py“ ändern

--- .github/workflows/api.py
+++ .github/workflows/api.py
@@ -8,4 +8,5 @@ gist_id = "73d8e030a44eb7f91bdeaea96a321f6d"
 
 content=open(filename, 'r').read()
 headers = {'Authorization': f'token {token}'}
-r = requests.patch('https://api.github.com/gists/' + gist_id, data=json.dumps({'files':{filename:{"content":content}}}),headers=headers) 
+r = requests.patch('https://api.github.com/gists/' + gist_id, data=json.dumps({'files':{filename:{"content":content}}}),headers=headers)
+print(r.json())
