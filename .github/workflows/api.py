9224079388f5e111f4c9ac4b6076ebdaee4071a7
Linux User <linuxusergd@noreply.codeberg.org>
2022-10-24 23:11:02 +0200

    „.github/workflows/api.py“ ändern

--- .github/workflows/api.py
+++ .github/workflows/api.py
@@ -5,20 +5,7 @@ import os
 token = os.environ["github_token"]
 gist_id = "73d8e030a44eb7f91bdeaea96a321f6d"
 
-filename = "main.py"
-content=open(filename, 'r').read()
-headers = {'Authorization': f'token {token}'}
-r = requests.patch('https://api.github.com/gists/' + gist_id, data=json.dumps({'files':{filename:{"content":content}}}),headers=headers)
-print(r.json())
-
-filename = "transpiler.py"
-content=open(filename, 'r').read()
-headers = {'Authorization': f'token {token}'}
-r = requests.patch('https://api.github.com/gists/' + gist_id, data=json.dumps({'files':{filename:{"content":content}}}),headers=headers)
-print(r.json())
-
-filename = "props.py"
-content=open(filename, 'r').read()
-headers = {'Authorization': f'token {token}'}
-r = requests.patch('https://api.github.com/gists/' + gist_id, data=json.dumps({'files':{filename:{"content":content}}}),headers=headers)
-print(r.json())
+for filename in ["main.py", "transpiler.py", "props.py"]:
+   content=open(filename, 'r').read()
+   headers = {'Authorization': f'token {token}'}
+   requests.patch('https://api.github.com/gists/' + gist_id, data=json.dumps({'files':{filename:{"content":content}}}),headers=headers)
