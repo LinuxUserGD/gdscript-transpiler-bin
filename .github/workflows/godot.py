e0945347ea14418801d63b2d3aa07972eeb5d072
Linux User <linuxusergd@noreply.codeberg.org>
2022-12-02 16:37:52 +0000

    „.github/workflows/godot.py“ ändern

--- .github/workflows/godot.py
+++ .github/workflows/godot.py
@@ -1,21 +1,2 @@
-from subprocess import call, TimeoutExpired
-
-timeLimit = 40  # seconds
-errMsg = ''
-ret_code = 0
-f = open('report.txt','w')
-
-try:
-    ret_code = call('D:\a\gdscript-transpiler-bin\gdscript-transpiler-bin\gdscript-transpiler-bin\godot.exe D:\a\gdscript-transpiler-bin\gdscript-transpiler-bin\gdscript-transpiler-bin\project.godot -v --headless --path "D:\a\gdscript-transpiler-bin\gdscript-transpiler-bin\gdscript-transpiler-bin" --export "Web" D:\a\gdscript-transpiler-bin\gdscript-transpiler-bin\gdscript-transpiler-bin\temp/index.html', stdout = f, stderr = f, timeout = timeLimit)
-
-except TimeoutExpired as e:
-    errMsg = 'Error: Time limit exceeded, terminating..'
-
-except Exception as e:
-    errMsg = 'Error: ' + str(e)
-
-if ret_code:
-    errMsg = 'Error: ' + str(ret_code)
-
-f.write(errMsg)
-f.close()
+import subprocess
+subprocess.run(['D:\a\gdscript-transpiler-bin\gdscript-transpiler-bin\gdscript-transpiler-bin\godot.exe', 'D:\a\gdscript-transpiler-bin\gdscript-transpiler-bin\gdscript-transpiler-bin\project.godot', '-v', '--headless', '--path', 'D:\a\gdscript-transpiler-bin\gdscript-transpiler-bin\gdscript-transpiler-bin', '--export', '"Web"', 'D:\a\gdscript-transpiler-bin\gdscript-transpiler-bin\gdscript-transpiler-bin\temp/index.html'], timeout=40)
