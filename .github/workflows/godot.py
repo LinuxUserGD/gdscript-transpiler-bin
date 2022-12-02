26d2224b8b36edac61762159956e4e2cb3bd643f
Linux User <linuxusergd@noreply.codeberg.org>
2022-12-02 17:17:45 +0000

    „.github/workflows/godot.py“ ändern

--- .github/workflows/godot.py
+++ .github/workflows/godot.py
@@ -1,3 +1,6 @@
 import subprocess
 cmd = ['D:/a/gdscript-transpiler-bin/gdscript-transpiler-bin/gdscript-transpiler-bin/godot.exe', 'D:/a/gdscript-transpiler-bin/gdscript-transpiler-bin/gdscript-transpiler-bin/project.godot', '-v', '--headless', '--path', 'D:/a/gdscript-transpiler-bin/gdscript-transpiler-bin/gdscript-transpiler-bin', '--export', '\"Web\"', 'D:/a/gdscript-transpiler-bin/gdscript-transpiler-bin/gdscript-transpiler-bin/temp/index.html']
-subprocess.run(cmd, timeout=40)
+try:
+    subprocess.run(cmd, timeout=40)
+except subprocess.TimeoutExpired:
+    print("done")
