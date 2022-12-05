64eefa18c847e73e35b7274faf8896a5e91b5c35
Linux User <linuxusergd@noreply.codeberg.org>
2022-12-05 14:17:52 +0000

    formatted props.gd using gdtoolkit

--- gdscript-transpiler-bin/props.gd
+++ gdscript-transpiler-bin/props.gd
@@ -7,11 +7,69 @@ class_name Props
 ## @tutorial(Generated python script): https://gist.github.com/LinuxUserGD/73d8e030a44eb7f91bdeaea96a321f6d
 
 ## Godot built-in types
-var types : Array = [ "AABB", "Array", "Basis", "bool", "Callable", "Color", "Dictionary", "float", "int", "max", "nil", "NodePath", "Object", "PackedByteArray", "PackedColorArray", "PackedFloat32Array", "PackedFloat64Array", "PackedInt32Array", "PackedInt64Array", "PackedStringArray", "PackedVector2Array", "PackedVector3Array", "Plane", "Quaternion", "Rect2", "Rect2i", "RID", "Signal", "String", "StringName", "Transform2D", "Transform3D", "Vector2", "Vector2i", "Vector3", "Vector3i", "void"] 
+var types: Array = [
+	"AABB",
+	"Array",
+	"Basis",
+	"bool",
+	"Callable",
+	"Color",
+	"Dictionary",
+	"float",
+	"int",
+	"max",
+	"nil",
+	"NodePath",
+	"Object",
+	"PackedByteArray",
+	"PackedColorArray",
+	"PackedFloat32Array",
+	"PackedFloat64Array",
+	"PackedInt32Array",
+	"PackedInt64Array",
+	"PackedStringArray",
+	"PackedVector2Array",
+	"PackedVector3Array",
+	"Plane",
+	"Quaternion",
+	"Rect2",
+	"Rect2i",
+	"RID",
+	"Signal",
+	"String",
+	"StringName",
+	"Transform2D",
+	"Transform3D",
+	"Vector2",
+	"Vector2i",
+	"Vector3",
+	"Vector3i",
+	"void"
+]
 ## Common gdscript operations (for parsing)
-var op : Array = [ "", ",", "[", "]", "+", "-", "*", "/", "+=", "-=", "*=", "/=", "=", "==", "!=", ">", "<", ">=", "<=" ]
+var op: Array = [
+	"",
+	",",
+	"[",
+	"]",
+	"+",
+	"-",
+	"*",
+	"/",
+	"+=",
+	"-=",
+	"*=",
+	"/=",
+	"=",
+	"==",
+	"!=",
+	">",
+	"<",
+	">=",
+	"<="
+]
 ## Dictionary to replace GDScript syntax with valid Python code
-var repl_dict : Dictionary = {
+var repl_dict: Dictionary = {
 	"-s": "",
 	"var": "",
 	"Node": "",
@@ -37,7 +95,8 @@ var repl_dict : Dictionary = {
 	"OS.execute('python',['-c','import": "",
 	"sys;print(sys.version)'],stdout,true,false)": "stdout = [sys.version]",
 	"OS.execute('python',['-c',import_str+": "",
-	"';print(Version.getNuitkaVersion())'],stdout,true,false)": "stdout = [Version.getNuitkaVersion()]",
+	"';print(Version.getNuitkaVersion())'],stdout,true,false)":
+	"stdout = [Version.getNuitkaVersion()]",
 	"quit()": "sys.exit()",
 	"self.quit()": "sys.exit()",
 	"#!/usr/bin/godot": "#!/usr/bin/env python",
@@ -49,18 +108,18 @@ var repl_dict : Dictionary = {
 	"player.stream": "~delete~",
 	"player.stream.data": "data",
 	"player.play()": "play(data)"
-	}
+}
 ## Print additional parsing information if true
-var debug : bool = true
+var debug: bool = true
 ## Print transpiled script as output to console
-var verbose : bool = false
+var verbose: bool = false
 ## Add additional python code for right() method to transpiled script if required
-var right_def : bool = false
+var right_def: bool = false
 ## Add additional python code for left() method to transpiled script if required
-var left_def : bool = false
+var left_def: bool = false
 ## Add additional python sys import to transpiled script if required
-var sys_imp : bool = true
+var sys_imp: bool = true
 ## Add additional python nuitka import to transpiled script if required
-var nuitka_imp : bool = true
+var nuitka_imp: bool = true
 ## Add additional python simpleaudio import to transpiled script if required
-var audio_imp : bool = true
+var audio_imp: bool = true
