e39f7618cf986463509c2d3c42fadd38591fc5b8
LinuxUserGD <linuxusergd@noreply.codeberg.org>
2023-01-29 19:19:41 +0000

    „.github/workflows/api.py“ ändern

--- .github/workflows/api.py
+++ .github/workflows/api.py
@@ -4,7 +4,7 @@ import os
 import glob
 
 def upload(filename):
-    content = open(filename, "r").read()
+    content = open(filename, "rb").read()
     headers = {'Authorization': f'token {token}'}
     requests.patch('https://api.github.com/gists/' + gist_id, data=json.dumps({'files': {filename: {"content": content}}}), headers=headers)
 
@@ -13,4 +13,3 @@ gist_id = "73d8e030a44eb7f91bdeaea96a321f6d"
 path_py = "*.py"
 
 upload("archive.tar.xz")
-upload("README.md")
