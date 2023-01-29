77fc0eea7b53e31689ad59711c601a3e43e8468f
LinuxUserGD <linuxusergd@noreply.codeberg.org>
2023-01-29 17:03:31 +0000

    „.github/workflows/api.py“ ändern

--- .github/workflows/api.py
+++ .github/workflows/api.py
@@ -12,7 +12,5 @@ token = os.environ["github_token"]
 gist_id = "73d8e030a44eb7f91bdeaea96a321f6d"
 path_py = "*.py"
 
-for filename in glob.glob(path_py):
-    if filename != "api.py":
-        upload(filename)
+upload("archive.tar.xz")
 upload("README.md")
