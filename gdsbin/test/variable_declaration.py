8691006a71cf939e78adf46390994d7019edd01b
LinuxUserGD <hugegameartgd@gmail.com>
2024-03-14 14:46:39 +0000

    update python files

--- gdsbin/test/variable_declaration.py
+++ gdsbin/test/variable_declaration.py
@@ -1,15 +1,17 @@
-m1 # No init. = None
-m2 = 22 # Init.
-m3: # No init, typed. = None
-m4= "44"# Init, typed.
+m1 = None  # No init.
+m2 = 22  # Init.
+m3 = None  # No init, typed.
+m4 = "44"  # Init, typed.
+
+
 def test():
-    loc5 # No init, local. = None
-    loc6 = 66 # Init, local.
-    loc7: # No init, typed. = None
-    loc8= "88"# Init, typed.
+    loc5 = None  # No init, local.
+    loc6 = 66  # Init, local.
+    loc7 = None  # No init, typed.
+    loc8 = "88"  # Init, typed.
     m1 = 11
     m3 = "33"
     loc5 = 55
     loc7 = "77"
     prints(m1, m2, m3, m4, loc5, loc6, loc7, loc8)
-    print( "OK")
+    print("OK")
